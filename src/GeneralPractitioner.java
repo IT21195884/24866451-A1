c865e482cca4826336b18cd523a1d3ffa225e517
IT21195884 <nelithhansindu@gmail.com>
2025-07-20 14:06:24 +0800

    GeneralPractitioner.java improved

--- src/GeneralPractitioner.java
+++ src/GeneralPractitioner.java
@@ -1,16 +1,19 @@
 public class GeneralPractitioner extends HealthProfessional {
     private String specialty;
 
+    //Default constructor featuring super class(HealthProfessional.java)
     public GeneralPractitioner() {
         super();
         this.specialty = "General Health";
     }
 
+    //Parametarized Constructor
     public GeneralPractitioner(String id, String name, String clinicLocation, String specialty) {
         super(id, name, clinicLocation);
         this.specialty = specialty;
     }
 
+    // Method to print general practitioner details
     @Override
     public void printDetails() {
         System.out.println("--- General Practitioner ---");
