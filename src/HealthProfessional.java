5e3991a951169ae91fd09859f0809ad4d36f066d
IT21195884 <nelithhansindu@gmail.com>
2025-07-20 14:02:50 +0800

    HealthProfessional.java improved

--- src/HealthProfessional.java
+++ src/HealthProfessional.java
@@ -3,18 +3,21 @@ public class HealthProfessional {
     protected String name;
     protected String clinicLocation;
 
+    //Default Constructor Initialized
     public HealthProfessional() {
         this.id = "";
         this.name = "";
         this.clinicLocation = "";
     }
 
+    //Parametarized Constructor
     public HealthProfessional(String id, String name, String clinicLocation) {
         this.id = id;
         this.name = name;
         this.clinicLocation = clinicLocation;
     }
 
+    //Health Professional Details printing method
     public void printDetails() {
         System.out.println("ID: " + id);
         System.out.println("Name: " + name);
