c0a57ef2f61c0be9a7ca779a8013418fd4131db4
IT21195884 <nelithhansindu@gmail.com>
2025-07-20 14:49:48 +0800

    Appointment.java & Dietitian.java Improved Classes

--- src/Appointment.java
+++ src/Appointment.java
@@ -4,6 +4,7 @@ public class Appointment {
     private String timeSlot;
     private HealthProfessional professional;
 
+    //Default Constructor
     public Appointment() {
         this.patientName = "";
         this.patientMobile = "";
@@ -11,6 +12,7 @@ public class Appointment {
         this.professional = null;
     }
 
+    //Parameterized Constructor
     public Appointment(String patientName, String patientMobile, String timeSlot, HealthProfessional professional) {
         this.patientName = patientName;
         this.patientMobile = patientMobile;
@@ -18,10 +20,12 @@ public class Appointment {
         this.professional = professional;
     }
 
+    // Getter for patient mobile number (used for cancellation)
     public String getPatientMobile() {
         return patientMobile;
     }
 
+    //Printing Appointment Details
     public void printAppointmentDetails() {
         System.out.println("Patient Name: " + patientName);
         System.out.println("Mobile: " + patientMobile);
--- src/Dietitian.java
+++ src/Dietitian.java
@@ -1,16 +1,20 @@
 public class Dietitian extends HealthProfessional {
     private String certification;
 
+    //Default constructor featuring super class(HealthProfessional.java)
     public Dietitian() {
         super();
         this.certification = "Registered Dietitian";
     }
 
+    //Parametarized Constructor
     public Dietitian(String id, String name, String clinicLocation, String certification) {
         super(id, name, clinicLocation);
         this.certification = certification;
     }
 
+
+    // Method to print dietitian details
     @Override
     public void printDetails() {
         System.out.println("--- Dietitian ---");
